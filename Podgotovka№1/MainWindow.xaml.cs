e0f84c57d5db3ddf7e8ed710bc24f13ccb2419d6
Кирилл <@DESKTOP-MPM09EH>
2024-03-29 21:01:29 +0500

    Добавьте файлы проекта.

---
+++ Podgotovka№1/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Podgotovka_1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+            
+        private void Edit(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void Enter(object sender, RoutedEventArgs e)
+        {
+            if (Login.Text != "" & Pass.Password == Pass2.Password)
+            {
+                List.Items.Add($"{Login.Text}");
+            }
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+
+                List.Items.Remove($"{Login.Text}");
+
+        }
+    }
+}
---
+++ Podgotovka№1/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podgotovka_1
+{
+    class User
+    {
+        public static List<User> Users = new List<User>();
+
+        public string Login { get; set; }
+        public User (string login)
+        {
+
+            Login = login;
+        }
+
+    }
+}
